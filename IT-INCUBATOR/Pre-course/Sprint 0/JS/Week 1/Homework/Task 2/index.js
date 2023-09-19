4c480929cd62e84f9b8509211ee5c427d7aa5229
dmtrsrgkzrv <dmtrsrgkzrv@gmail.com>
2023-09-19 13:58:53 +0400

    Trying to fix bug

--- IT-INCUBATOR/Pre-course/Sprint 0/JS/Week 1/Homework/Task 2/index.js
+++ IT-INCUBATOR/Pre-course/Sprint 0/JS/Week 1/Homework/Task 2/index.js
@@ -1,6 +1,8 @@
-let number = Math.floor(Math.random() * 100)
+body {
+    let number = Math.floor(Math.random() * 100)
 if (number % 2 == 0) {
     console.log(number, "Это четное число");
 } else {
     console.log(number, "Это нечетное число");
+}
 }
