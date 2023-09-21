b9d3b8899f21047c95ff435742068bc109b8904a
dmtrsrgkzrv <dmtrsrgkzrv@gmail.com>
2023-09-21 18:00:52 +0400

    Complete task

---
+++ IT-INCUBATOR/Pre-course/Sprint 0/JS/Lessons/Week 2/1.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Напишите функцию isPositive, которая принимает один аргумент - число, и возвращает true, если число положительное,
+// и false в противном случае. Используйте условный оператор if и стрелочную функцию.
+
+// SOLUTION:
+const isPositive = arg  => {
+    if (arg > 0) {
+        return "True";
+    } else {
+        return "False";
+    }
+}
