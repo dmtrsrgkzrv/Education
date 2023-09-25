5be33583484c21d1f89d0354b101fe0b63f0ca8f
dmtrsrgkzrv <dmtrsrgkzrv@gmail.com>
2023-09-25 23:50:06 +0400

    Edit file

---
+++ IT-INCUBATOR/Pre-course/Sprint 0/JS/Homework/lesson-02/07.js
@@ -0,0 +1,13 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let passportMarriedCopy = { ...passportMarried, married: true, address: { ...passportMarried.address } };
+
+console.log(passportMarried);
+console.log(passportMarriedCopy);
