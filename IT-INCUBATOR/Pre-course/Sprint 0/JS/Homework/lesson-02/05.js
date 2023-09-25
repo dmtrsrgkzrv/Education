e780fcb57956f754f2ad17b9b15894f8bf7205bc
dmtrsrgkzrv <dmtrsrgkzrv@gmail.com>
2023-09-25 23:22:34 +0400

    Complete task

---
+++ IT-INCUBATOR/Pre-course/Sprint 0/JS/Homework/lesson-02/05.js
@@ -0,0 +1,10 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let passportCopy = {...passport};
+passportCopy.name = "Ivan";
+
+console.log(passport);
+console.log(passportCopy);
