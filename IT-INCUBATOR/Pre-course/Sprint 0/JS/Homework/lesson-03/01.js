4d9415b894c19fae2b7aad52324ead415144a8d7
dmtrsrgkzrv <dmtrsrgkzrv@gmail.com>
2023-09-28 23:02:18 +0400

    Edit file

--- IT-INCUBATOR/Pre-course/Sprint 0/JS/Homework/lesson-03/01.js
+++ IT-INCUBATOR/Pre-course/Sprint 0/JS/Homework/lesson-03/01.js
@@ -2,6 +2,6 @@ const colors = ["синий", "черный", "зеленый", "красный"
 
 const createColorString = colors.filter(function (color) {
     return color === "черный" || color === "красный" || color === "желтый";
-});
+}).join("-");
 
-console.log("'"+createColorString.join("-")+"'");
+console.log(createColorString);
