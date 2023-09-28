4fc218c220e7fd43233a5a6db6d6d4904dff2d73
dmtrsrgkzrv <dmtrsrgkzrv@gmail.com>
2023-09-28 23:16:33 +0400

    Edit file

--- IT-INCUBATOR/Pre-course/Sprint 0/JS/Homework/lesson-03/01.js
+++ IT-INCUBATOR/Pre-course/Sprint 0/JS/Homework/lesson-03/01.js
@@ -1,8 +1,10 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
-const createColorString = colors
-    .filter(function (color) {
+
+function createColorString() {
+    return colors.filter(function (color) {
         return color === "черный" || color === "красный" || color === "желтый";
     })
     .join("-");
+}
 
-console.log(createColorString);
+console.log(createColorString());
