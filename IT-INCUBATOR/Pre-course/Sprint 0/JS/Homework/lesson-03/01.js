cc820509c75363b9c187653e4c454f5ef15de228
dmtrsrgkzrv <dmtrsrgkzrv@gmail.com>
2023-09-28 22:44:29 +0400

    Complete task

---
+++ IT-INCUBATOR/Pre-course/Sprint 0/JS/Homework/lesson-03/01.js
@@ -0,0 +1,7 @@
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+const createColorString = colors.filter(function (color) {
+    return color === "черный" || color === "красный" || color === "желтый";
+});
+
+console.log(createColorString.join("-"));
